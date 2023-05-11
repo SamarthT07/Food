c4b227ec2cb084270be42a3014b28c17b7fd67c2
Samarth Tate <101213490+SamarthT07@users.noreply.github.com>
2023-05-11 21:53:02 +0530

    js

--- main.js
+++
@@ -1,8 +0,0 @@
-const callToActionBtns = document.querySelectorAll(".mobile__CTA--btn");
-
-callToActionBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    callToActionBtns.forEach(f => f != e.target ? f.classList.remove('active') : '');
-      e.target.classList.toggle("active");
-  });
-});
